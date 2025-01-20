e473e62142d11775e144cd32dd9377eec2ebf700
imran0developer <imran0developer@gmail.com>
2025-01-21 00:43:18 +0530

    meta data updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "V99",
-  description: "Nearby Location",
+  description: "Create your online presence with V99",
 };
 
 export default function RootLayout({
